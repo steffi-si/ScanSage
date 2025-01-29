e2ffe9a4a4c80c74e178b85d955cf4a8676d7b64
Stefanie <multa.excogitare@gmail.com>
2025-01-29 09:12:04 +0100

    add warehous routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import deliveryRoutes from "./routes/deliveryRoutes.js";
+import warehousRoutes from "./routes/warehouseRoutes.js";
 import connectDB from "./config/db.js";
 import cors from "cors";
 
@@ -20,5 +21,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/deliveries", deliveryRoutes);
+app.use("/api/warehouses", warehousRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
